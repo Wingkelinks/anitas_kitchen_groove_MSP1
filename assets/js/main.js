8a0f3b55b9f6c7de14cf2a4ce444f3444dd1cfc5
Whitney Turner <whitters.t@gmail.com>
2021-01-31 10:08:30 +1000

    Tidied up code, updated readme.md

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,18 +17,3 @@ $(document).ready(function () {
 		easing: "linear",
 	});
 });
-
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function () {
-	scrollFunction();
-};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		document.getElementById("nav").style.fontSize = "30px";
-	} else {
-		document.getElementById("nav").style.fontSize = "90px";
-	}
-}
-
-
